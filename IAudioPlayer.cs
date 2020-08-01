1f6ed541a950378fca382e0fbcc7957036b6133a
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-31 21:24:40 -0400

    Add project files.

---
+++ IAudioPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_10_4
+{
+
+    // Step 1: Create an interface for playing audio media. We'll create a new interface that has 
+    //      one method Play().
+    interface IAudioPlayer
+    {
+        void Play();
+    }
+}
+
